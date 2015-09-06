7fa4abaf38f89aab5d094905f35820c55e9d9abc
Boris JC <boris@hrseed.com>
2015-09-06 02:03:19 +0000

    update new theme

---
+++ www/wp-content/themes/drharnet-blue-bg2/footer.php
@@ -0,0 +1,27 @@
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div>
+                   <ul class="ulbar">
+                    <li><a href="#" title="Terms & Conditions">Terms & Conditions of use </a></li>
+                    <li><a href="#" title="Privacy Policy">Privacy Policy </a></li>
+                    <li><a target="_blank" href="#" title="Dr Harnet">&copy; 2015 Dr Harnet. All right reserved</a> </li>
+                    </ul>
+                </div>
+                <div>
+               <ul class="social">
+                                    <li><a  target="_blank" href="https://www.linkedin.com/pub/harnet-bokrezion-phd/70/a87/3a1"><i class="icon-linkedin icon-social"></i> </a></li>
+                                    <li><a target="_blank" href="http://twitter.com/http://twitter.com/https://twitter.com/africajumpstart"><i class="icon-twitter icon-social"></i> </a></li>
+                    
+                                      <li><a  target="_blank" href="https://www.facebook.com/pages/Africa-Business-Jumpstart/745276662163866?skip_nax_wizard=true"><i class="icon-facebook icon-social"></i> </a></li>
+                                       <li><a target="_blank" href="https://plus.google.com/106553478086304757836/posts"><i class="icon-google-plus icon-social"></i> </a></li>
+                  </ul>
+                </div>
+               
+            </div>
+        </div>
+    </footer><!--/#footer-->
+ 
+  </body>
+</html>
