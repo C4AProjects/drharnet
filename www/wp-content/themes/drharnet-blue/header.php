e32032b2cd09990aa446a707800672b7289c0e29
Boris JC <boris@hrseed.com>
2015-09-10 19:39:03 +0000

    update images

--- www/wp-content/themes/drharnet-blue/header.php
+++ www/wp-content/themes/drharnet-blue/header.php
@@ -26,7 +26,7 @@
     <script src="<?php echo  get_template_directory_uri().'/js/html5shiv.js'; ?>"></script>
       <script src="<?php echo  get_template_directory_uri().'/js/respond.min.js'; ?>"></script>
     <![endif]-->
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="shortcut icon" href="<?php echo  get_template_directory_uri().'/images/ico/favicon.ico'; ?>">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo  get_template_directory_uri().'/images/ico/apple-touch-icon-144-precomposed.png'; ?>">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo  get_template_directory_uri().'/images/ico/apple-touch-icon-114-precomposed.png'; ?>">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo  get_template_directory_uri().'/images/ico/apple-touch-icon-72-precomposed.png'; ?>">
